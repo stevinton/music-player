113d9045e189a9152801ab9ae98b84d363af8875
stevinton <stevinton9@gmail.com>
2024-02-29 22:01:57 +0530

    final

--- script.js
+++ script.js
@@ -18,8 +18,16 @@ const removeExist = (t) => {
     t.classList.toggle("hidden");
 };
 
-const back = (val) =>{
-    val -= val
-    console.log(val)
-    // document.querySelector(`song-${va}`)
+const back = function (val) {
+    document.querySelector(`.song-${val}`).classList.toggle("hidden");
+    val = (val === 1) ? 8 : val - 1;
+    document.querySelector(`.song-${val}`).classList.toggle("hidden");
+    return true
+}
+
+const forward = (val) => {
+    document.querySelector(`.song-${val}`).classList.toggle("hidden");
+    val = (val === 8) ? 1 : val + 1
+    document.querySelector(`.song-${val}`).classList.toggle("hidden");
+    return true
 }
